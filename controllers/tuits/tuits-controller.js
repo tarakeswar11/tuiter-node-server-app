f75cc226e426e6ffd524ebffb589c8c1207eb094
Tarakeswar Nallamothu <59277224+tarakeswar11@users.noreply.github.com>
2023-06-15 01:23:22 -0400

    v4

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,32 +1,34 @@
-import * as tuitsDao from './tuits-dao.js'
+import * as tuitsDao from "./tuits-dao.js";
 
-const createTuit = async(req, res) => {const newTuit = req.body;
-    
-    newTuit.likes = 0;
-    newTuit.liked = false;
-    newTuit.title = 'The National Aeronautics and Space Administration is an independent agency of the U.S. federal government responsible for the civil space program';
-    const insertedTuit = await tuitsDao.createTuit(insertedTuit);
-    res.json(newTuit);
-  }
-const findTuits  = async(req, res) => {
-  const tuits = await tuitsDao.findTuits()
-  res.json(tuits);}
+const createTuit = async (req, res) => {
+  const newTuit = req.body;
+  newTuit.likes = 0;
+  newTuit.liked = false;
+  const insertedTuit = await tuitsDao.createTuit(newTuit);
+  res.json(insertedTuit);
+};
 
-const updateTuit = async(req, res) => {const tuitdId = req.params.tid;
-    const updates = req.body;
-    const status = await tuitsDao.updateTuit(tuitdIdToUpdate, updates);
-    res.json(status);}
+const findTuits = async (req, res) => {
+  const tuits = await tuitsDao.findTuits();
+  res.json(tuits);
+};
 
-const deleteTuit = async(req, res) => {
+const updateTuit = async (req, res) => {
+  const tuitdIdToUpdate = req.params.tid;
+  const updates = req.body;
+  const status = await tuitsDao.updateTuit(tuitdIdToUpdate, updates);
+  res.json(status);
+};
+
+const deleteTuit = async (req, res) => {
   const tuitdIdToDelete = req.params.tid;
   const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
-
-  res.json(status);}
+  res.json(status);
+};
 
 export default (app) => {
- app.post('/api/tuits', createTuit);
- app.get('/api/tuits', findTuits);
- app.put('/api/tuits/:tid', updateTuit);
- app.delete('/api/tuits/:tid', deleteTuit);
-}
-
+  app.post("/api/tuits", createTuit);
+  app.get("/api/tuits", findTuits);
+  app.put("/api/tuits/:tid", updateTuit);
+  app.delete("/api/tuits/:tid", deleteTuit);
+};
