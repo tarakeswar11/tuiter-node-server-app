8d2c3a76bd39c01c7b5e65ef7d4345f3a08e3d06
Tarakeswar Nallamothu <59277224+tarakeswar11@users.noreply.github.com>
2023-06-15 00:49:11 -0400

    22

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -1,7 +1,15 @@
 import mongoose from 'mongoose';
 const schema = mongoose.Schema({
+  topic: String,
+  userName: String,
+  title: String,
+  time: String,
+  image: String,
+  replies: Number,
+  retuits: Number,
   tuit: String,
   likes: Number,
+  handle: String,
   liked: Boolean,
 }, {collection: 'tuits'});
 export default schema;
