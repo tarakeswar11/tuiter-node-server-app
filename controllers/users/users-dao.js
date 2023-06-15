8aaf4ceeff9d306636491f518182d676739ba087
Tarakeswar Nallamothu <59277224+tarakeswar11@users.noreply.github.com>
2023-06-15 12:04:00 -0400

    user schema update

--- controllers/users/users-dao.js
+++ controllers/users/users-dao.js
@@ -1,48 +1,22 @@
-import people from './users.js'
-
-let users = people
+import usersModel from "./users-model.js";
 
 
-export const findAllUsers = () => users;
+export const findAllUsers = () => usersModel.find();
 
 
-export const findUserById = (uid) => {
-    const index = users.findIndex((u) => u._id === uid);
-    if (index !== -1) return users[index];
-    return null;
-};
+export const findUserById = (uid) => usersModel.findById(id);
 
 
-export const findUserByUsername = (username) => {
-    const index = users.findIndex((u) => u.username === username);
-    if (index !== -1) return users[index];
-    return null;
-};
+export const findUserByUsername = (username) => usersModel.findOne({ username });
 
 
-export const findUserByCredentials = (username, password) => {
-    const index = users.findIndex((u) => u.username === username && u.password === password);
-    if (index !== -1) return users[index];
-    return null;
-};
+export const findUserByCredentials = (username, password) => usersModel.findOne({ username, password });
 
 
-export const createUser = (user) => {
-    const newUser = {...user, _id: (new Date()).getTime() + ''}
-    users.push(newUser);
-    return newUser;
-}
+export const createUser = (user) => usersModel.create(user);
 
 
-export const updateUser = (uid, user) => {
-    const index = users.findIndex((u) => u._id === uid);
-    users[index] = { ...users[index], ...user };
-    return users[index] 
-};
+export const updateUser = (uid, user) => usersModel.updateOne({ _id: id }, { $set: user });
 
 
-export const deleteUser = (uid) => {
-    const index = users.findIndex((u) => u._id === uid);
-    users.splice(index, 1);
-    return { status: 'ok' }
-};
+export const deleteUser = (uid) => usersModel.deleteOne({ _id: id });
