cf05ee988f426b65b40b20a7cc25019ffb16b1c5
Tarakeswar Nallamothu <59277224+tarakeswar11@users.noreply.github.com>
2023-06-15 12:43:36 -0400

    users fix

--- controllers/users/users-dao.js
+++ controllers/users/users-dao.js
@@ -4,7 +4,7 @@ import usersModel from "./users-model.js";
 export const findAllUsers = () => usersModel.find();
 
 
-export const findUserById = (uid) => usersModel.findById(id);
+export const findUserById = (id) => usersModel.findById(id);
 
 
 export const findUserByUsername = (username) => usersModel.findOne({ username });
@@ -16,7 +16,7 @@ export const findUserByCredentials = (username, password) => usersModel.findOne(
 export const createUser = (user) => usersModel.create(user);
 
 
-export const updateUser = (uid, user) => usersModel.updateOne({ _id: id }, { $set: user });
+export const updateUser = (id, user) => usersModel.updateOne({ _id: id }, { $set: user });
 
 
-export const deleteUser = (uid) => usersModel.deleteOne({ _id: id });
+export const deleteUser = (id) => usersModel.deleteOne({ _id: id });
